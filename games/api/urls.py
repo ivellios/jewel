b9a9aae0d606fb79f1cbf7410592b1bdfc56d7cb
Janusz "Ivellios" Kamieński <janusz.kamienski@gmail.com>
2025-08-16 18:17:18 +0200

    extend the API to provide list of games and the search functionality + add tests

--- games/api/urls.py
+++ games/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GameDetailAPIView
+from .views import GameListAPIView, GameDetailAPIView
 
 urlpatterns = [
+    path('', GameListAPIView.as_view(), name='game-list-api'),
     path('<uuid:id>/', GameDetailAPIView.as_view(), name='game-detail-api'),
 ]
--- games/api/views.py
+++ games/api/views.py
@@ -1,8 +1,18 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter
 from ..models import Game
 from .serializers import GameSerializer
 from .authentication import APITokenAuthentication
+
+
+class GameListAPIView(generics.ListAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    authentication_classes = [APITokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ['title']
 
 
 class GameDetailAPIView(generics.RetrieveAPIView):
